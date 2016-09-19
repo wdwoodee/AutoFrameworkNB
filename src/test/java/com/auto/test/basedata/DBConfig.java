f0653e8f37e7941da265a89b2b8c95d545ac90d8
luozt <18511246771@163.com>
2016-09-19 21:04:12 +0800

    sssss

---
+++ src/test/java/com/auto/test/basedata/DBConfig.java
@@ -0,0 +1,14 @@
+package com.auto.test.basedata;
+/**  
+ * 类说明   
+ *  
+ * @author luozt  
+ * @date 2016年9月19日  新建  
+ */
+public interface DBConfig {
+
+	public final static String mongo_ip="10.10.4.213";
+	public final static int mongo_port=27017;
+	public final static String mongo_dbname="test_api";
+	
+}
