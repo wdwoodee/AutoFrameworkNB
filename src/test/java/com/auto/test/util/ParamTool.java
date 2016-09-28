9c7221919cfcb538226964a4a582a8f1f35a5565
luozt <18511246771@163.com>
2016-09-28 17:10:36 +0800

    paramTool

--- src/test/java/com/auto/test/util/ParamTool.java
+++ src/test/java/com/auto/test/util/ParamTool.java
@@ -54,17 +54,6 @@ public class ParamTool {
 		mapprams.put("prefix", "sea");
 		mapprams.put("count", 10);
 		int [] aa=new int[]{0,1,2,3,4,5,6,7,8,9};
-		ArrayList<Integer>test=new ArrayList<Integer>();
-		test.add(0);
-		test.add(1);
-		test.add(2);
-		test.add(3);
-		test.add(4);
-		test.add(5);
-		test.add(6);
-		test.add(7);
-		test.add(8);
-		test.add(9);
 		mapprams.put("types",aa);
 		return mapprams;
 		
