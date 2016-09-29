11d15a292dee9c9b8bb974ed8ff74a38daffad5b
luozt <18511246771@163.com>
2016-09-29 14:54:04 +0800

    read_writecsv

--- src/test/java/com/auto/test/util/CompareCSV.java
+++ src/test/java/com/auto/test/util/CompareCSV.java
@@ -1,10 +1,68 @@
 package com.auto.test.util;
 
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
+import com.csvreader.CsvReader;
+import com.csvreader.CsvWriter;
+
 /**
- * @author gaohuan
+ * @author luozt
  *
  * 2016年9月12日下午8:35:09
  */
 public class CompareCSV {
-
+	
+	
+	/**
+	 * 
+	 * 读取csv
+	 */
+	public static final String ENCODE="UTF-8";
+	ArrayList<String[]> csvList=new ArrayList<String[]>();
+	
+    public ArrayList<String[]> readCsv(String csvPath){
+    	
+    	try{
+    		
+    		String csvpath="";
+    		CsvReader reader=new CsvReader(csvpath,',',Charset.forName(ENCODE));
+    		reader.readHeaders();//跳过表头，如果需要表头的话，不要写这句
+    		
+    		while(reader.readRecord()){//逐行读入除表头的数据
+    			
+    			csvList.add(reader.getValues());
+    			
+    		}
+    		reader.close();
+    		
+    		
+    	}catch(Exception e){
+    		e.printStackTrace();
+    	}
+    	
+    	return csvList;
+    	
+    }
+    
+    
+    
+    /**
+     * 
+     * 写入文件
+     * 
+     */
+    
+    public static void WriteCsv(String csvPath,String []contents){
+    	
+    	try{
+    		
+    		CsvWriter wr=new CsvWriter(csvPath,',',Charset.forName(ENCODE));
+    		wr.writeRecord(contents);;
+    		wr.close();
+    		
+    	}catch(Exception e){e.printStackTrace();}
+    	
+    	
+    }
 }
